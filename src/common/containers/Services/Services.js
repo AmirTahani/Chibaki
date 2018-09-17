ca8ed21b5e220dc01303725ab6d0e90e83385193
Ali Atik <ali1wg@gmail.com>
2018-09-17 15:55:27 +0430

    addde : services page

--- src/common/containers/Services/Services.js
+++ src/common/containers/Services/Services.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router';
-import '../Home/Home.css';
+import '../Services/Services.css';
+import { Row, Col } from 'antd';
+import { connect } from 'react-redux';
+import 'antd/dist/antd.css'
+import professions from "../../redux/modules/professions";
 
-export default class About extends React.Component {
+
+class Services extends React.Component {
     render() {
-        const { name } = this.props;
+        const { name, cat } = this.props;
+        console.log(cat);
         return (
-            <div className="Home">
-                <div className="Home-header">
-                    <h2>About page</h2>
-                </div>
-                <p>
-                    <Link to="home">{name}</Link>
-                </p>
+            <div className="Home Container">
+                {cat.map((item)=>{
+                    return(
+                        <div >
+                            <h2 className="services_heading">{item.category}</h2>
+                            <Row>
+                            {item.professions.map((it)=>{
+                                return(
+
+                                    <Col xs={24} sm={12} md={8} lg={8} xl={8} xxl={6}>
+                                        <h3 className='title'>
+                                            <Link to={`/${encodeURI('خدمات')}/${it.title.split(' ').join('_')}`}>{it.title}</Link>
+                                        </h3>
+                                    </Col>
+                                )
+                            })}
+                            </Row>
+                        <div className='seprator'/>
+                        </div>
+
+                    )
+                })}
             </div>
         );
     }
 }
+export default connect(
+    state => ({
+        cat: state.professions.categories
+    }),
+    undefined
+)(Services)
