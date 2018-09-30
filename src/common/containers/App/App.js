d4be00ddbb9fca8eac548719e8a4a95c1b7023f4
Jopp <jopp@Jopps-MacBook-Pro.local>
2018-09-30 16:54:18 +0330

    fix bug

--- src/common/containers/App/App.js
+++ src/common/containers/App/App.js
@@ -1,23 +1,23 @@
 import "./App.styl";
 import '../../styles/icon.styl'
 
-import { Router, Route, browserHistory, Professional } from "react-router";
+import { Router, Route, browserHistory } from "react-router";
 import React from "react";
 
-import { Home, About, Services, Tos, Service } from "../";
+import { Home, About, Services, Tos, Service, Professional } from "../";
 
 const Routes = props => {
-	return (
-		<Router history={browserHistory} {...props}>
-			<Route path="/" component={Home} />
-			<Route path="home" component={Home} />
-			<Route path="about" component={About} />
-			<Route path="tos" component={Tos} />
-			<Route exact path={encodeURI("خدمات")} component={Services} />
-			<Route path={`${encodeURI("خدمات")}/:title`} component={Service} />
-			<Route path={`professional/:id`} component={Professional} />
-		</Router>
-	);	
+    return (
+        <Router history={browserHistory} {...props}>
+            <Route path="/" component={Home} />
+            <Route path="home" component={Home} />
+            <Route path="about" component={About} />
+            <Route path="tos" component={Tos} />
+            <Route exact path={encodeURI("خدمات")} component={Services} />
+            <Route path={`${encodeURI("خدمات")}/:title`} component={Service} />
+            <Route path={`professional/:id`} component={Professional} />
+        </Router>
+    );
 };
 
 export default Routes;
