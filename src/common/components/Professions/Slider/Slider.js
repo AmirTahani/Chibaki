8d76e717523bc561f7ba7abeb42da1a44c8e31a5
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2018-10-02 13:14:18 +0330

    Add profession sliders

--- src/common/components/Professions/Slider/Slider.js
+++ src/common/components/Professions/Slider/Slider.js
@@ -1,17 +1,132 @@
+import "./Slider.styl";
+
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { Button } from "antd";
+import objectFitImages from "object-fit-images";
 
 export default class ProfessionSliders extends Component {
 	static propTypes = {
-		sliders: PropTypes.arrayOf(PropTypes.object)
+		sliders: PropTypes.arrayOf(
+			PropTypes.object
+		)
+	};
+
+	IS_WEB = typeof window !== "undefined";
+
+	componentWillMount() {
+		if (this.IS_WEB) {
+			require("flickity");
+			require("flickity/dist/flickity.min.css");
+		}
+	}
+
+	componentDidMount() {
+		objectFitImages();
+	}
+
+	sliderOptions = {
+		lazyLoad: 1,
+		pageDots: true,
+		cellSelector: ".catSlider__slide",
+		rightToLeft: true,
+		wrapAround: true,
+		groupCells: true,
+		percentPosition: false,
+		selectedAttraction: 0.015,
+		friction: 0.2,
+		freeScroll: false
+	};
+
+	onBtnClick = profId => {
+		console.log(profId);
 	};
 
 	render() {
 		console.log(this.props.sliders);
-		return <div>{this.props.sliders.map(slider => {
-			<h2>
-				{ slider.label }
-			</h2>
-		})}</div>;
+
+		const getSlider = slider => {
+			return (
+				<div
+					className="catSlider"
+					data-flickity={JSON.stringify(
+						this.sliderOptions
+					)}
+				>
+					{slider.slides.map(mapSlides)}
+				</div>
+			);
+		};
+
+		const mapSlides = (slide, idx) => {
+			return (
+				<div
+					className="catSlider__slide"
+					key={idx}
+				>
+					<div className="catSlider__item">
+						<div className="catSlider-item__inner">
+							<div className="catSlider__bg">
+								<img
+									data-flickity-lazyload={
+										slide.img
+									}
+									alt={
+										slide.label
+									}
+								/>
+							</div>
+
+							<div className="catSlider__over">
+								<Button
+									type="primary"
+									className="catSlider__btn"
+									onClick={() =>
+										this.onBtnClick(
+											slide._id
+										)
+									}
+								>
+									<div>
+										<span>
+											ثبت
+											درخواست
+										</span>
+									</div>
+								</Button>
+							</div>
+						</div>
+						<div className="catSlider__label">
+							<h3>{slide.label}</h3>
+							<div className="catSlider__price">
+								{slide.price}
+							</div>
+						</div>
+					</div>
+				</div>
+			);
+		};
+
+		const sliders = this.props.sliders.map(
+			(slider, idx) => {
+				return (
+					<div
+						className="catSlider__wrapper"
+						key={idx}
+					>
+						<div>
+							<h2 className="catSlider__heading">
+								{slider.title}
+							</h2>
+						</div>
+						<div className="catSlider__outer">
+							{getSlider(slider)}
+						</div>
+					</div>
+				);
+			}
+		);
+
+		return <div>{sliders}</div>;
 	}
 }
--- src/common/containers/Home/Home.js
+++ src/common/containers/Home/Home.js
@@ -1,17 +1,19 @@
 import "./Home.css";
+import "../../styles/container.styl"
 
 import { connect } from "react-redux";
 import React, { Component } from "react";
 
 import { flattenProfessionsByCategories } from "../../utils/serverHelper";
-import Categories from '../../components/Professions/Categories/Categories';
+import Categories from "../../components/Professions/Categories/Categories";
+import ProfessionSliders from "../../components/Professions/Slider/Slider";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 
 class Home extends Component {
 	render() {
-		let { professions } = this.props;
+		let { professions, sliders } = this.props;
 		professions = flattenProfessionsByCategories(
 			professions
 		);
@@ -21,7 +23,15 @@ class Home extends Component {
 
 				<Hero professions={professions} />
 
-				<Categories />
+				<div className="l-container l-container--md">
+					<Categories />
+				</div>
+
+				<div className="l-container l-container--sm">
+					<ProfessionSliders
+						sliders={sliders}
+					/>
+				</div>
 
 				{/* <Questions /> */}
 				<Footer />
@@ -37,5 +47,6 @@ class Home extends Component {
 }
 
 export default connect(state => ({
-	professions: state.professions.categories
+	professions: state.professions.categories,
+	sliders: state.professions.professionsList
 }))(Home);
