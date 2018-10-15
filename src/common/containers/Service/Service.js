8d547f5dc85119a386ffe75f02a77756757e759d
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2018-10-15 17:37:21 +0330

    Improve Service layout

--- src/common/containers/Service/Service.js
+++ src/common/containers/Service/Service.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import { Row, Col, Tooltip, Button } from 'antd';
+import objectFitImages from 'object-fit-images';
 import { connect } from 'react-redux';
-import { Row, Col } from 'antd';
-import '../Services/Services.css';
-import './ServiceStyle.css';
+
+import styles from './ServiceStyle.module.css';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 class Services extends Component {
     static propTypes = {
@@ -16,6 +19,10 @@ class Services extends Component {
         ProjectsForProfession: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired
     };
 
+    componentDidMount() {
+        objectFitImages();
+    }
+
     registerProject = () => {
         console.log(this.props.selectedProfession, 'askjdakjdbasjkldb');
     };
@@ -24,84 +31,103 @@ class Services extends Component {
         const { proficients, title, selectedProfession, count, provinces, ProjectsForProfession } = this.props;
         console.log(ProjectsForProfession);
         return (
-            <div>
-                <Row type={'flex'} gutter={30}>
-                    <Col span={24} className="header-image_container">
-                        <img src="https://chibaki.ir/assets/images/hero/architect.jpg" alt="architect" />
-                        <div className={'absolute-cover'}>
-                            <h1 className={'page-title'}> {title}</h1>
-                            <button onClick={this.registerProject}>
-                                ثبت درخواست
-                            </button>
-                        </div>
-                    </Col>
-                </Row>
-                <Row type={'flex'}>
-                    <Col span={24}>
-                        <div>
-                            <button onClick={this.registerProject}>
-                                ثبت درخواست
-                            </button>
-                        </div>
-                        <div>
-                            متخصصین
-                            {' '}
-                            {title}
-                            {' '}
-                            در چی‌باکی (
-                            {count}
-                            متخصص)
-                        </div>
-                        <span>نمایش تضادفی</span>
-                    </Col>
-                </Row>
-                {proficients.map((item) => {
-                    return (
-                        <Row type={'flex'} align={'middle'} justify={'center'}>
-                            <Link to={`/profession/${item.id}`}>
-                                <Col span={8}>
-                                    <Row>
-                                        <Col span={4}>
-                                            <span>badge</span>
-                                        </Col>
-                                        <Col span={20}>
-                                            <Row>
-                                                <Col span={24}>
-                                                    {/*<img*/}
-                                                    {/*src={`https://chibaki.ir/${item.profilePicture.filePath.replace('public', '')}`}*/}
-                                                    {/*className="prof-image"*/}
-                                                    {/*alt="user profile"*/}
-                                                    {/*/>*/}
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <Col span={24}>
-                                                    <span>{item.firstname}{' '}{item.lastname}</span>
-                                                </Col>
-                                            </Row>
+            <div className={styles.wrapper}>
+                <Header />
 
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col span={16}>
-                                    <Row>
-                                        {item.introDescription}
-                                    </Row>
-                                    <Row>
-                                        <button>
-                                            مشاهده پروفایل
-                                        </button>
-                                    </Row>
-                                </Col>
-                            </Link>
-                        </Row>
-                    );
-                })}
-                <div>
-                    <button>
-                        مشاهده بیشتر
-                    </button>
+                <div className={styles.hero}>
+                    <div className={styles.heroBg}>
+                        <img src="https://chibaki.ir/assets/images/hero/architect.jpg" alt="Smiley face" />
+                    </div>
+                    <div className={styles.heroContent}>
+                        <div>
+                            <h1 className={styles.heroTitle}>{title}</h1>
+                            <Button type="primary" size="large" onClick={this.registerProject}>
+                                ثبت درخواست
+                            </Button>
+                        </div>
+                    </div>
                 </div>
+
+                <div className={`l-container ${styles.container}`}>
+
+                    <div className={styles.price}>
+                        شروع قیمت در چی باکی: از جلسه‌ای
+                    </div>
+
+                    <div className={styles.desc}>
+                        {selectedProfession.description}
+                    </div>
+
+                    <Row type="flex">
+                        <Col span={24}>
+                            <div>
+                                <Button onClick={this.registerProject}>
+                                    ثبت درخواست
+                                </Button>
+                            </div>
+                            <div>
+                                متخصصین
+                                {' '}
+                                {title}
+                                {' '}
+                                در چی‌باکی (
+                                {count}
+                                {' '}
+                                متخصص)
+                            </div>
+                            <span>نمایش تضادفی</span>
+                        </Col>
+                    </Row>
+                    {proficients.map((item) => {
+                        return (
+                            <Row type={'flex'} align={'middle'} justify={'center'}>
+                                <Link to={`/profession/${item.id}`}>
+                                    <Col span={8}>
+                                        <Row>
+                                            <Col span={4}>
+                                                <span>badge</span>
+
+                                            </Col>
+                                            <Col span={20}>
+                                                <Row>
+                                                    <Col span={24}>
+                                                        <img
+                                                            src={item.profilePicture && `https://chibaki.ir/${item.profilePicture.filePath.replace('public', '')}`}
+                                                            className={'prof-image'}
+                                                        />
+
+                                                    </Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col span={24}>
+                                                        <span>{`${item.firstname} ${item.lastname}`}</span>
+                                                    </Col>
+                                                </Row>
+
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col span={16}>
+                                        <Row>
+                                            {item.introDescription}
+                                        </Row>
+                                        <Row>
+                                            <button>
+                                                مشاهده پروفایل
+                                            </button>
+                                        </Row>
+                                    </Col>
+                                </Link>
+                            </Row>
+                        );
+                    })}
+                    <div>
+                        <button>
+                            مشاهده بیشتر
+                        </button>
+                    </div>
+                </div>
+                <Footer />
             </div>
         );
     }
@@ -116,4 +142,3 @@ export default connect(
         provinces: state.provinces.provinces,
         ProjectsForProfession: state.ProjectsForProfession.ProjectsForProfession
     }))(Services);
-
