832dd8a2f18b9aa6b22c784ac970efb6a3245f3b
Jopp <jopp@Jopps-MacBook-Pro.local>
2018-11-17 13:41:14 +0330

    change sitepath

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,6 @@
 // export const apiPath = 'https://api-staging.chibaki.ir';
-export const apiPath = 'https://api.chibaki.ir';
-export const sitePath = 'https://chibaki.ir';
+export const apiPath = 'https://api.chibaki.co';
+export const sitePath = 'https://chibaki.co';
 
 export const defualtQuestions = [
     {
