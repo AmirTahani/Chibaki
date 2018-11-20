768a53b1dd9455804ec22573523c122b98f728ad
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2018-11-20 14:58:27 +0330

    Fix validation on login input Store phoneNumberRegex in utils/persian

--- src/common/utils/persian.js
+++ src/common/utils/persian.js
@@ -50,3 +50,5 @@ export function latinNumber(input) {
 }
 
 export const persianRegex = '^[\u0020\u2000-\u200F\u2028-\u202F\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC\u06F0-\u06F9\u060C\u061B\u061F\u0640\u066A\u066B\u066C\u0629\u0643\u0649-\u064B\u064D\u06D5]+$';
+
+export const phoneNumberRegex = /^(09)\d{9}$/;
