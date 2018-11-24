dbe35b8f9aaea02a39676e3a7308b13ecefa047c
Sepehr Hosseini <iamsepehrhosseini@gmail.com>
2018-11-24 11:16:06 +0330

    Fix underscore char in sliders url

--- src/common/components/Professions/Slider/Slider.js
+++ src/common/components/Professions/Slider/Slider.js
@@ -50,7 +50,7 @@ export default class ProfessionSliders extends Component {
                 className="catSlider__slide"
                 key={idx}
             >
-                <Link to={`/${encodeURI('خدمات')}/${slide.label.replace(' ', '_')}`} className="catSlider__item">
+                <Link to={`/${encodeURI('خدمات')}/${slide.label.split(' ').join('_')}`} className="catSlider__item">
                     <div className="catSlider-item__inner">
                         <div className="catSlider__bg">
                             <img
