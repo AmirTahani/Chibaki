d2457991e40a4fbf3874533a3d15c9743ff605cd
Jopp <jopp@Jopps-MacBook-Pro.local>
2018-12-09 14:21:31 +0330

    creating config depends on serverside buidling

--- src/common/redux/create.js
+++ src/common/redux/create.js
@@ -5,19 +5,19 @@ import storage from 'redux-persist/lib/storage';
 import reducers from './reducers';
 import saga from './saga';
 
-const config = {
-    key: 'primary',
-    storage,
-    whitelist: [
-        'auth', 'provinces', 'professions'
-    ],
-};
 export default function create(client, preloadState, type) {
     const sagaMiddleWare = createSagaMiddleware();
     let finalReducers;
     if (type === 'server') {
         finalReducers = reducers;
     } else {
+        const config = {
+            key: 'primary',
+            storage,
+            whitelist: [
+                'auth', 'provinces', 'professions'
+            ],
+        };
         finalReducers = persistReducer(config, reducers);
     }
     const store = createStore(finalReducers, preloadState, applyMiddleware(sagaMiddleWare));
